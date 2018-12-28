4a0db1afd71b6980a1bc5111b71ea67026b579c5
Diego Gonzalez <diego.gonzalez@enxendra.com>
2018-12-28 11:08:57 +0100

    Updates

---
+++ precache-manifest.cd075bded13caa80bd7ee184e402213a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b114f00da5c633b5b1c2",
+    "url": "/docuten-blockchain-proof-dapp/static/css/main.72a66e3d.chunk.css"
+  },
+  {
+    "revision": "b114f00da5c633b5b1c2",
+    "url": "/docuten-blockchain-proof-dapp/static/js/main.b114f00d.chunk.js"
+  },
+  {
+    "revision": "500d881d891c64e4ff16",
+    "url": "/docuten-blockchain-proof-dapp/static/js/1.500d881d.chunk.js"
+  },
+  {
+    "revision": "15b79285e06cbeb81dd8",
+    "url": "/docuten-blockchain-proof-dapp/static/js/runtime~main.15b79285.js"
+  },
+  {
+    "revision": "015137a749aad3e94327c4f1e59060e9",
+    "url": "/docuten-blockchain-proof-dapp/index.html"
+  }
+];
