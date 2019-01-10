dbd4ab2ec22be7703c35728df55f5593161b1a56
Diego Gonzalez <diego.gonzalez@enxendra.com>
2019-01-10 14:10:30 +0100

    Allow proxy deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var ProofOfLife = artifacts.require("./ProofOfLife.sol");
+var ProofOfLifeProxy = artifacts.require("./ProofOfLifeProxy.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ProofOfLife);
+  deployer.deploy(ProofOfLife).then(
+    function() {
+      return deployer.deploy(ProofOfLifeProxy, ProofOfLife.address);
+    }
+  );
 };
