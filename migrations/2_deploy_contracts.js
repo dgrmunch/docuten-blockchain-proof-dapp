a89e0838d8a5c3df9d46e8905dd62637f11ba985
Diego Gonzalez <diego.gonzalez@enxendra.com>
2019-01-15 15:09:47 +0100

    Update deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,27 @@
 var ProofOfLife = artifacts.require("./ProofOfLife.sol");
 var ProofOfLifeProxy = artifacts.require("./ProofOfLifeProxy.sol");
 
-module.exports = function(deployer) {
+var proxy;
+var delegate;
+
+module.exports =  function(deployer){
   deployer.deploy(ProofOfLife).then(
-    function() {
-      return deployer.deploy(ProofOfLifeProxy, ProofOfLife.address);
+     function() {
+      return ProofOfLife.deployed();
+    }).then(async function(instance){
+      delegate = instance;
+      console.log('\n\t -- The ProofOfLifeProxy instance will point to the delegate address :')
+      console.log('\n\t\t'+ProofOfLife.address);
+      console.log('\n\t -- This will allow ProofOfLifeProxy to know where is the delegate contract (ProofOfLife) deployed');
+      return deployer.deploy(ProofOfLifeProxy, ProofOfLife.address).then(
+        function() {
+          return ProofOfLifeProxy.deployed();
+        }).then(async function() {
+          
+          console.log('\n\n\n\t Delegate: '+ProofOfLife.address);
+          console.log('\t Proxy: '+ProofOfLifeProxy.address);
+      }); 
     }
   );
+  
 };
