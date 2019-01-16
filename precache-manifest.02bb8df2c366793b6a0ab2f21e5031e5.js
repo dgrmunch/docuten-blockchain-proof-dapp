00798e38110c5d0a25b6f8564b8cac4a48e4c02f
Diego Gonzalez <diego.gonzalez@enxendra.com>
2019-01-16 12:42:50 +0100

    Updates

--- precache-manifest.02bb8df2c366793b6a0ab2f21e5031e5.js
+++ precache-manifest.02bb8df2c366793b6a0ab2f21e5031e5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "997a6b99ce5cd6cf1fca",
+    "revision": "79a6a45694fc0cfe172e",
     "url": "/docuten-blockchain-proof-dapp/static/css/main.72a66e3d.chunk.css"
   },
   {
-    "revision": "997a6b99ce5cd6cf1fca",
-    "url": "/docuten-blockchain-proof-dapp/static/js/main.997a6b99.chunk.js"
+    "revision": "79a6a45694fc0cfe172e",
+    "url": "/docuten-blockchain-proof-dapp/static/js/main.79a6a456.chunk.js"
   },
   {
     "revision": "1d8f1739313f7a0418eb",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/docuten-blockchain-proof-dapp/static/js/runtime~main.15b79285.js"
   },
   {
-    "revision": "f3545b81a8c18fe179dc6d01181a949f",
+    "revision": "2ed311e3ed320869f1af4219d5f27782",
     "url": "/docuten-blockchain-proof-dapp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/docuten-blockchain-proof-dapp/precache-manifest.f9d22e0083d1cbb066124918878c4821.js"
+  "/docuten-blockchain-proof-dapp/precache-manifest.02bb8df2c366793b6a0ab2f21e5031e5.js"
 );
 
 workbox.clientsClaim();
