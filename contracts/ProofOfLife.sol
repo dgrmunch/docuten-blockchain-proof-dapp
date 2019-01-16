ac7c1c1a4fcd80e83973ed9f3416b04492d6c13b
Diego Gonzalez <diego.gonzalez@enxendra.com>
2019-01-16 12:20:47 +0100

    BC-21 Remove public functions from ProofOfLife (storage will be in proxy)

--- contracts/ProofOfLife.sol
+++ contracts/ProofOfLife.sol
@@ -44,7 +44,7 @@ contract ProofOfLife  is ProofOfExistence, HasAuditRegistry {
 
     /* @notice Certify document creation
     * @dev It registers in the blockchain the proof-of-existence of an external document
-    * @param _documentHash - MD5 Hash of the document (it should have 32 bytes)
+    * @param _documentHash - Hash of the document (it should have 32 bytes)
     * @param _ipfsHash - IPFS Hash, if it exists 
     * @return _id - returns certified document id
     */
@@ -94,47 +94,5 @@ contract ProofOfLife  is ProofOfExistence, HasAuditRegistry {
         
     }
 
-    /* @notice Get auditRegistry by documentHash
-    * @dev Retrieves the audit registry of a document
-    * @param _documentHash - Hash of the document
-    * @param _index - Position of the auditRegistry in the list
-    * @return string description, string timestamp, uint256 blockTimestamp
-    */
-    function getAuditRegistryByDocumentHash(string memory _documentHash, uint256 _index) public view
-    returns (string memory, string memory, uint256) {
-        uint256 _id = getId(_documentHash);
-        return getAuditRegistryByDocumentId(_id, _index);
-    }
-      
-    /* @notice Get auditRegistry by documentId
-    * @dev Retrieves the audit registry of a document
-    * @param _id - Id of the document
-    * @param _index - Position of the auditRegistry in the list
-    * @return string description, string timestamp, uint256 blockTimestamp
-    */
-    function getAuditRegistryByDocumentId(uint256 _id, uint256 _index) public view
-    returns (string memory, string memory, uint256) {
-        return (auditRegistryByDocumentId[_id][_index].description,
-        auditRegistryByDocumentId[_id][_index].timestamp, auditRegistryByDocumentId[_id][_index].blockTimestamp);
-    }
-      
-    /* @notice CountAuditRegistries by hash
-    * @dev Count the number of audit registries of a document
-    * @param _documentHash - Hash of the document
-    * @return uint256 number of elements
-    */
-    function countAuditRegistriesByDocumentHash(string memory _documentHash) public view returns(uint256) {
-        uint256 _id = getId(_documentHash);
-        return auditRegistryByDocumentId[_id].length;
-    }
-
-    /* @notice CountAuditRegistries by id
-    * @dev Count the number of audit registries of a document
-    * @param _id - Id of the document
-    * @return uint256 number of elements
-    */
-    function countAuditRegistriesByDocumentId(uint256 _id) public view returns(uint256) {
-        return auditRegistryByDocumentId[_id].length;
-    }
 
 }
